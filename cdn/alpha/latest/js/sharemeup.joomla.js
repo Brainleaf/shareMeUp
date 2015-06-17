cce976e7abb168fa3d308aa2b7525518c30fa736
Gix075 <gildo.giuliani@gmail.com>
2015-06-18 00:03:41 +0200

    Update sharemeup.joomla.js

--- cdn/alpha/latest/js/sharemeup.joomla.js
+++ cdn/alpha/latest/js/sharemeup.joomla.js
@@ -1,9 +1,9 @@
 var shareMeUp = function (params) {
     
     this.params = params;
-    this.params.css = "css/sharemeup.min.css";
-    this.params.plugin = "js/jquery.sharemeup.js";
-    this.params.imgPath = "img/sharemeup/"
+    this.params.css = "http://brainleaf.github.io/shareMeUp/cdn/alpha/latest/css/sharemeup.min.css";
+    this.params.plugin = "http://brainleaf.github.io/shareMeUp/cdn/alpha/latest/js/jquery.sharemeup.min.js";
+    this.params.imgPath = "http://brainleaf.github.io/shareMeUp/cdn/alpha/latest/img/sharemeup/"
     
     this.init = function () {
         jQuery('body').one().prepend('<link rel="stylesheet" href="'+ this.params.css +'">');
