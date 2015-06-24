7f9753e4654cf24389e3a21b5a4df43e0cd07ce5
Gix075 <gildo.giuliani@gmail.com>
2015-06-24 03:45:18 +0200

    Site and CDN update
    Site and CDN update

--- cdn/v0/beta/js/sharemeup.joomla.js
+++ cdn/v0/beta/js/sharemeup.joomla.js
@@ -1,6 +1,6 @@
    /*! 
     *  jQueryShareMeUp | contents sharing widget 
-    *  Version 0.9.2-beta - CDN - 2015-06-21 
+    *  Version 0.10.0-beta - CDN - 2015-06-24 
     *  HomePage: https://github.com/Brainleaf/shareMeUp 
    */ 
-var shareMeUp=function(a){function b(a){jQuery.ajax({url:a.plugin,success:function(){jQuery(".sharemeup").shareMeUp({size:a.size,style:a.style,imgPath:a.imgPath})},error:function(){alert("ShareMeUp: Ajax Error!")}})}this.params=a,this.params.css="https://brainleaf.github.io/shareMeUp/cdn/v0/beta/css/sharemeup.min.css",this.params.plugin="https://brainleaf.github.io/shareMeUp/cdn/v0/beta/js/jquery.sharemeup.min.js",this.params.imgPath="https://brainleaf.github.io/shareMeUp/cdn/v0/beta/img/sharemeup/",this.init=function(){jQuery("body").one().prepend('<link rel="stylesheet" href="'+this.params.css+'">'),this.params.async===!0?jQuery(window).on("load",b(this.params)):jQuery(document).on("ready",b(this.params))}};
+var shareMeUp=function(a){function b(a){jQuery.ajax({url:a.plugin,success:function(){jQuery(".sharemeup").shareMeUp({size:a.size,style:a.style,imgPath:a.imgPath})},error:function(){alert("ShareMeUp: Ajax Error!")}})}this.params=a,this.cdn="https://brainleaf.github.io/shareMeUp/cdn/v0/beta/",this.params.css=this.cdn+"css/sharemeup.min.css",this.params.plugin=this.cdn+"js/jquery.sharemeup.min.js",this.params.imgPath=this.cdn+"img/sharemeup/",this.init=function(){jQuery("body").one().prepend('<link rel="stylesheet" href="'+this.params.css+'">'),this.params.async===!0?jQuery(window).on("load",b(this.params)):jQuery(document).on("ready",b(this.params))}};
